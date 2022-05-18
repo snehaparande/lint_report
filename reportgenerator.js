458b14f44c71ffdd2f1ba5f8b5396ead3eaaee47
Sneha Parande <99521196+snehaparande@users.noreply.github.com>
2022-05-18 12:25:40 +0530

    Creating html representaion of the lint report in json fomat.   added tables for each file report.   added styles for tables in 'styles.css'.   made two functions for creating table head and rows.   changed the order of arguments of 'createTag' function.     takes ('tagName', 'content', 'attrubutes') as input.

--- reportgenerator.js
+++ reportgenerator.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
-const { createTag, attributes, link } = require('./tools.js');
+const { createTag,
+  attributes,
+  link,
+  createTableHead,
+  createTableRow
+} = require('./tools.js');
 
 const totalOf = (field, report) => {
   return report.reduce((total, fileReport) => {
@@ -7,38 +12,66 @@ const totalOf = (field, report) => {
   }, 0);
 };
 
-const overview = (report) => {
+const overview = report => {
   const totalErrors = 'Errors: ' + totalOf('errorCount', report);
-  const errors = createTag('p', '', totalErrors);
+  const errors = createTag('p', totalErrors);
   const totalWarnings = 'Warnings: ' + totalOf('warningCount', report);
-  const warnings = createTag('p', '', totalWarnings);
+  const warnings = createTag('p', totalWarnings);
 
   const attrs = [
     { attr: 'class', value: 'overview' },
   ];
-  return createTag('div', attributes(attrs), errors + warnings);
+  return createTag('div', errors + warnings, attributes(attrs));
 };
 
-const pageWrapper = (report) => {
-  const heading = createTag('h1', '', 'Lint Report');
-  const header = createTag('header', '', heading + overview(report));
+const createTableBody = errors => {
+  const head = createTableHead(['Line', 'Column', 'Error message']);
+  const data = errors.map(({ line, column, message }) => {
+    return createTableRow([line, column, message]);
+  }).join('');
+  return createTag('tbody', head + data);
+};
+
+const fileReport = ({ filePath, messages }) => {
+  const caption = createTag('caption', filePath.match(/[^/]*$/)[0]);
+  const tBody = createTableBody(messages);
+  return createTag('table', caption + tBody);
+};
+
+const pageContent = reports => {
+  const attrs = [
+    { attr: 'class', value: 'report_tables' },
+  ];
+  const tables = reports.map(fileReport);
+  return createTag('div', tables.join(''), attributes(attrs));
+};
+
+const pageWrapper = report => {
+  const heading = createTag('h1', 'Lint Report');
+  const header = createTag('header', heading + overview(report));
+  const content = pageContent(report);
+
   const attrs = [
     { attr: 'class', value: 'page_wrapper' },
   ];
-  return createTag('div', attributes(attrs), header);
+  return createTag('div', header + content, attributes(attrs));
 };
 
-const createPage = function (report) {
-  const headContent = createTag('title', '', 'Lint Report') + link();
-  const head = createTag('head', '', headContent);
-  const body = createTag('body', '', pageWrapper(report));
-  return createTag('html', '', head + body);
+const createPage = report => {
+  const headContent = createTag('title', 'Lint Report') + link();
+  const head = createTag('head', headContent);
+  const body = createTag('body', pageWrapper(report));
+  return createTag('html', head + body);
 };
 
 const reportGenerator = () => {
-  let report = fs.readFileSync('./report.json', 'utf8');
-  report = JSON.parse(report);
-  fs.writeFileSync('./index.html', createPage(report), 'utf8');
+  try {
+    let report = fs.readFileSync('./report.json', 'utf8');
+    report = JSON.parse(report);
+    fs.writeFileSync('./index.html', createPage(report), 'utf8');
+  } catch (error) {
+    throw error.message;
+  }
 };
 
 reportGenerator();
--- tools.js
+++ tools.js
@@ -2,8 +2,9 @@ const openingTag = (tag, attributes) => '<' + tag + ' ' + attributes + '>';
 
 const closingTag = tag => '</' + tag + '>';
 
-const createTag = function (tag, attributes, content) {
-  return openingTag(tag, attributes) + content + closingTag(tag);
+const createTag = function (tag, content, attributes) {
+  const attrs = attributes ? attributes : '';
+  return openingTag(tag, attrs) + content + closingTag(tag);
 };
 
 const attribute = ({ attr, value }) => {
@@ -14,8 +15,20 @@ const attributes = (attrs) => {
   return attrs.map(attr => attribute(attr)).join(' ');
 };
 
+const createTableHead = (headers) => {
+  const tableHeader = headers.map((header) => createTag('th', header));
+  return createTag('tr', tableHeader.join(''));
+};
+
+const createTableRow = (rowData) => {
+  const tableRowData = rowData.map((data) => createTag('td', data));
+  return createTag('tr', tableRowData.join(''));
+};
+
 const link = () => '<link rel="stylesheet" href="styles.css"/>';
 
 exports.createTag = createTag;
 exports.attributes = attributes;
+exports.createTableHead = createTableHead;
+exports.createTableRow = createTableRow;
 exports.link = link;
